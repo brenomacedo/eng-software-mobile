0b0cf6fdaf2cc94c6b6f8e4cda83ec80765139c1
Breno Macêdo <b88660501@gmail.com>
2023-06-27 21:12:31 -0300

    changing to production

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.0.105:3333/api/v1',
+  baseURL: 'https://pickapal-backend.onrender.com/api/v1',
   timeout: 10000
 });
 
