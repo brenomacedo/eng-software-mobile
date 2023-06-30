38bf98c27f5929f8d65aca2fdaea06c5fd5d8e0e
Breno Macêdo <b88660501@gmail.com>
2023-06-30 18:23:55 -0300

    eslint

--- src/screens/EventRequests/EventRequests.jsx
+++ src/screens/EventRequests/EventRequests.jsx
@@ -1,65 +1,42 @@
-import {useEffect, useState} from 'react';
-import { View, ScrollView, TouchableOpacity, Text, StyleSheet, Image , Alert} from 'react-native';
-import { ArrowBack, MapInput } from '../../components';
+import { useEffect, useState } from 'react';
+import { View, ScrollView, TouchableOpacity, Text, Alert } from 'react-native';
 import styles from './styles';
 import api from '../../api';
 import useAuth from '../../hooks/useAuth';
 
-const colorirStatus = (indice) => {
+const colorirStatus = indice => {
+  const estados = {
+    0: { nome: 'Rejeitado', cor: 'red' },
+    1: { nome: 'Pendente', cor: 'blue' },
+    2: { nome: 'Aceito', cor: 'green' }
+  };
 
-    const estados = {0: {nome:"Rejeitado", cor:"red"}, 
-                     1: {nome:"Pendente", cor:"blue"},
-                     2: {nome:"Aceito", cor:"green"}};
+  const status = estados[indice];
 
-    const status = estados[indice];
+  return <Text style={{ color: status.cor }}>{status.nome}</Text>;
+};
 
-
-    return (<Text style={{color:status.cor}}>{status.nome}</Text>)
-}
-
-const Button = function(data, index){
-
-  return(
+const Button = function (data, index) {
+  return (
     <TouchableOpacity key={index} style={styles.button}>
       <View style={styles.buttonText}>
-                <Text style={styles.buttonTextTitle}>
-                    {data.Titulo}
-                </Text>
-                <Text style={styles.buttonTextSubtitle}>
-                    {data.Local}
-                </Text>
-                <Text style={styles.buttonTextSubtitle}>
-                    {data.Horario}
-                </Text>
-                <Text style={styles.buttonTextSubtitle}>
-                    Status: {colorirStatus(data.status)}
-                </Text>
+        <Text style={styles.buttonTextTitle}>{data.Titulo}</Text>
+        <Text style={styles.buttonTextSubtitle}>{data.Local}</Text>
+        <Text style={styles.buttonTextSubtitle}>{data.Horario}</Text>
+        <Text style={styles.buttonTextSubtitle}>
+          Status: {colorirStatus(data.status)}
+        </Text>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-const EventRequests = () => {
+const EventRequests = ({ navigation }) => {
   const [requests, setRequests] = useState([]);
   const [loading, setLoading] = useState(true);
   const { authToken, logout } = useAuth();
 
-
-  //Recebe do backend as informações, 
-  let buttons = [{Titulo: 'Título do evento', Local: 'Local', Horario:'16:30-17:30',status:1,bellCount:5}, 
-                 {Titulo: 'Olimpiada', Local: 'Estônia', Horario:'29:30-47:30',status:0,bellCount:23},
-                 {Titulo: 'Show de fogos', Local: 'Coreia do Norte',status:2,Horario:'19:87-35:83',bellCount:12}
-                ];
-
-  //Teste para multiplos botões
-  buttons = buttons.concat(buttons).concat(buttons);
-  buttons = buttons.concat(buttons).concat(buttons)
-  // buttons = buttons.concat(buttons).concat(buttons)
-
-
   useEffect(() => {
-
-
     /*
       Request {
     id: 2,
@@ -84,30 +61,37 @@ const EventRequests = () => {
     */
 
     const findUserRequests = async () => {
-      
       let newRequestsArray = [];
       try {
-        const response = await api.get('/request/me', {
-              headers: {
+        const response = await api
+          .get('/request/me', {
+            headers: {
               authorization: `Bearer ${authToken}`
-            }}).then(res => res.data);
-        console.log(response);
-        response.map((request) => {
+            }
+          })
+          .then(res => res.data);
+
+        response.map(request => {
           let newObject = {
             Titulo: request.title,
             Local: request.location,
-            Horario: `${request.start_time.slice(0,5)}-${request.end_time.slice(0,5)}`,
-            status: (request.status == 'PENDING') ? 1 : ( (request.status =='ACCEPTED') ? 2 : 0)
-          }
+            Horario: `${request.start_time.slice(
+              0,
+              5
+            )}-${request.end_time.slice(0, 5)}`,
+            status:
+              request.status == 'PENDING'
+                ? 1
+                : request.status == 'ACCEPTED'
+                ? 2
+                : 0
+          };
           newRequestsArray.push(newObject);
-        })
-          
-        setRequests(newRequestsArray);
-        
-        
-        setLoading(false);
+        });
 
-      }catch(err) {
+        setRequests(newRequestsArray);
+        setLoading(false);
+      } catch (err) {
         if (err.response && err.response.status === 401) {
           Alert.alert('Erro', 'Sessão expirada');
           logout().then(() => {
@@ -123,43 +107,31 @@ const EventRequests = () => {
         ) {
           Alert.alert('Erro', err.response.data.error);
         }
-      } 
+      }
+    };
 
-      
-        
-
-        }
-
-     findUserRequests();   
-
-
-
-  },[])
+    findUserRequests();
+  }, []);
 
   return (
-      <ScrollView 
-        style={styles.scroll} 
-        contentContainerStyle={styles.eventDetailsContainer}
-      >
+    <ScrollView
+      style={styles.scroll}
+      contentContainerStyle={styles.eventDetailsContainer}
+    >
+      <Text style={styles.eventDetailsInfoTitle}>Solicitações</Text>
 
-        <Text style={styles.eventDetailsInfoTitle}>Solicitações</Text>
-
-        {
-          loading ? (
-            <Text style={styles.loading}>Carregando...</Text>
-          ) : ( 
-            (requests.length > 0) ?
-            (requests.map((data, index) => Button(data,index))) :
-            (<Text style={styles.noResults}> Nenhum resultado encontrado</Text>) 
-          )
-        }
-        
-      </ScrollView>
+      {loading ? (
+        <Text style={styles.loading}>Carregando...</Text>
+      ) : requests.length > 0 ? (
+        requests.map((data, index) => Button(data, index))
+      ) : (
+        <Text style={styles.noResults}> Nenhum resultado encontrado</Text>
+      )}
+    </ScrollView>
   );
-}; 
+};
 
 export default EventRequests;
-
 
 /*
 
--- src/screens/UserEventRequestsScreen/UserEventRequestsScreen.jsx
+++ src/screens/UserEventRequestsScreen/UserEventRequestsScreen.jsx
@@ -1,13 +1,13 @@
-import { ArrowBack, MapInput } from '../../components';
+import { ArrowBack } from '../../components';
 import styles from './styles';
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Alert } from 'react-native';
 
 import useAuth from '../../hooks/useAuth';
 import api from '../../api';
 
-const Message = function({ data, index, onMessageAction, loading }) {
+const Message = function ({ data, index, onMessageAction, loading }) {
   const handleAccept = () => {
     onMessageAction(data, index, 'ACCEPTED');
   };
@@ -22,54 +22,55 @@ const Message = function({ data, index, onMessageAction, loading }) {
         <Text style={styles.messageTextTitle}>{data.Nome}</Text>
         <Text style={styles.messageTextSubtitle}>{data.Mensagem}</Text>
 
-        {data.status == "PENDING" ? 
-          (
-            <View style={styles.buttonGroup}> 
-              <TouchableOpacity
-                disabled={loading}
-                style={{ ...styles.button, backgroundColor: loading ? "gray" : '#58D7B8' }}
-                onPress={handleAccept}
-              >
-                <Text style={styles.buttonText}>Aceitar</Text>
-              </TouchableOpacity>
-    
-              <TouchableOpacity
-                disabled={loading}
-                style={{ ...styles.button, backgroundColor: loading ? "gray" : '#F90000' }}
-                onPress={handleReject}
-              >
-                <Text style={styles.buttonText}>Recusar</Text>
-              </TouchableOpacity>
-            </View>
-          ) : 
-          (
-            (data.status == 'ACCEPTED') ?
-            (<Text style={styles.confirmMessage}>Solicitação já foi aceita.</Text>) :
-            (<Text style={styles.confirmMessage}>Solicitação foi recusada.</Text>)
-          )
-        }
+        {data.status == 'PENDING' ? (
+          <View style={styles.buttonGroup}>
+            <TouchableOpacity
+              disabled={loading}
+              style={{
+                ...styles.button,
+                backgroundColor: loading ? 'gray' : '#58D7B8'
+              }}
+              onPress={handleAccept}
+            >
+              <Text style={styles.buttonText}>Aceitar</Text>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+              disabled={loading}
+              style={{
+                ...styles.button,
+                backgroundColor: loading ? 'gray' : '#F90000'
+              }}
+              onPress={handleReject}
+            >
+              <Text style={styles.buttonText}>Recusar</Text>
+            </TouchableOpacity>
+          </View>
+        ) : data.status == 'ACCEPTED' ? (
+          <Text style={styles.confirmMessage}>Solicitação já foi aceita.</Text>
+        ) : (
+          <Text style={styles.confirmMessage}>Solicitação foi recusada.</Text>
+        )}
       </View>
     </View>
   );
 };
 
-const UserEventRequestsScreen = ({navigation, route}) => {
+const UserEventRequestsScreen = ({ navigation, route }) => {
+  const { event_id } = route.params;
 
-  const {event_id} = route.params;
-
-
-
-  //Recebe do backend as informações, 
-  let eventName = "Racha"
-  let info = [{Nome: "Gustavo Wendell", Mensagem:"Aceita aí"}, {Nome: "Wendell Gustavo", Mensagem:"Rejeita aí"}
-                ];  
+  //Recebe do backend as informações,
+  let eventName = 'Racha';
+  let info = [
+    { Nome: 'Gustavo Wendell', Mensagem: 'Aceita aí' },
+    { Nome: 'Wendell Gustavo', Mensagem: 'Rejeita aí' }
+  ];
 
   //Teste para multiplos botões
   info = info.concat(info).concat(info);
   info = info.concat(info).concat(info);
   // info = info.concat(info).concat(info);
-  
+
   const initialMessages = info;
 
   //Hook pra remover as mensagens de solicitação aceitam/rejeitam
@@ -77,24 +78,26 @@ const UserEventRequestsScreen = ({navigation, route}) => {
   const [loading, setLoading] = useState(true);
   const [reqLoading, setReqLoading] = useState(false);
 
-  const {authToken} = useAuth();
+  const { authToken, logout } = useAuth();
 
   useEffect(() => {
-    setMessages(initialMessages)
-
+    setMessages(initialMessages);
 
     const getRequests = async () => {
       setLoading(true);
       let newRequestsArray = [];
       try {
-        const response = await api.get(`/request/all/${event_id}`, {
-              headers: {
+        const response = await api
+          .get(`/request/all/${event_id}`, {
+            headers: {
               authorization: `Bearer ${authToken}`
-            }}).then(res => res.data);
-        response.map((request) => {
+            }
+          })
+          .then(res => res.data);
+        response.map(request => {
           let newObject = {
             Nome: request.name,
-            Mensagem:request.message,
+            Mensagem: request.message,
             id: request.id,
             status: request.status
           };
@@ -103,7 +106,7 @@ const UserEventRequestsScreen = ({navigation, route}) => {
         setMessages(newRequestsArray);
 
         setLoading(false);
-      }catch(err) {
+      } catch (err) {
         if (err.response && err.response.status === 401) {
           Alert.alert('Erro', 'Sessão expirada');
           logout().then(() => {
@@ -120,76 +123,81 @@ const UserEventRequestsScreen = ({navigation, route}) => {
           Alert.alert('Erro', err.response.data.error);
         }
       }
-    }
+    };
 
     getRequests();
-
-
-
-
   }, []);
 
   const handleMessageAction = async (data, index, action) => {
     setReqLoading(true);
-    
 
     try {
-        const response = await api.patch(`/request/${data.id}`, {
-          answer: action
-        }, {
-              headers: {
+      const response = await api
+        .patch(
+          `/request/${data.id}`,
+          {
+            answer: action
+          },
+          {
+            headers: {
               authorization: `Bearer ${authToken}`
-            }}).then(res => res.data);
+            }
+          }
+        )
+        .then(res => res.data);
 
-
-        
-        if (response) {
-          Alert.alert('Sucesso!', 'Requisição respondida!');
-          let newArray = [...messages];
-          newArray[newArray.indexOf(data)].status = action;
-          setMessages(newArray);
-        }
-
-
-        setReqLoading(false);
-      }catch(err) {
-        if (err.response && err.response.status === 401) {
-          Alert.alert('Erro', 'Sessão expirada');
-          logout().then(() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'LoginScreen' }]
-            });
-          });
-        } else if (
-          err.response &&
-          err.response.data &&
-          err.response.data.error
-        ) {
-          Alert.alert('Erro', err.response.data.error);
-        }
+      if (response) {
+        Alert.alert('Sucesso!', 'Requisição respondida!');
+        let newArray = [...messages];
+        newArray[newArray.indexOf(data)].status = action;
+        setMessages(newArray);
       }
+
+      setReqLoading(false);
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        Alert.alert('Erro', 'Sessão expirada');
+        logout().then(() => {
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'LoginScreen' }]
+          });
+        });
+      } else if (err.response && err.response.data && err.response.data.error) {
+        Alert.alert('Erro', err.response.data.error);
+      }
+    }
   };
 
   return (
-    <ScrollView style={styles.scroll} contentContainerStyle={styles.eventDetailsContainer}>
-       <ArrowBack onPress={() => {}} />
-      <Text style={styles.eventDetailsInfoTitle}>Solicitações de {eventName}</Text>
-        {
-          loading ? (
-            <Text>Carregando...</Text>
-          ) : ( 
-            (messages.length > 0) ?
-            (messages.map((data, index) => (<Message key={index} data={data} index={index} onMessageAction={handleMessageAction} loading={reqLoading} />))) :
-            (<Text style={styles.noResults}> Nenhum resultado encontrado</Text>) 
-          )
-        }
+    <ScrollView
+      style={styles.scroll}
+      contentContainerStyle={styles.eventDetailsContainer}
+    >
+      <ArrowBack onPress={() => {}} />
+      <Text style={styles.eventDetailsInfoTitle}>
+        Solicitações de {eventName}
+      </Text>
+      {loading ? (
+        <Text>Carregando...</Text>
+      ) : messages.length > 0 ? (
+        messages.map((data, index) => (
+          <Message
+            key={index}
+            data={data}
+            index={index}
+            onMessageAction={handleMessageAction}
+            loading={reqLoading}
+          />
+        ))
+      ) : (
+        <Text style={styles.noResults}> Nenhum resultado encontrado</Text>
+      )}
     </ScrollView>
   );
 };
 
 export default UserEventRequestsScreen;
-
 
 /*
 
