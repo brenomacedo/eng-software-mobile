7f251cf46c211f5008c80c29a553ce9c1060b09f
Breno Macêdo <b88660501@gmail.com>
2023-06-30 22:22:06 -0300

    removing bell count

--- src/screens/MyEvents/MyEvents.jsx
+++ src/screens/MyEvents/MyEvents.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { View, ScrollView, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
-import { ArrowBack, MapInput } from '../../components';
+import { View, ScrollView, TouchableOpacity, Text, Image } from 'react-native';
 import styles from './styles';
 
-const Button = function(data, index){
-
-  return(
+const Button = function (data, index) {
+  return (
     <TouchableOpacity key={index} style={styles.button}>
       <View style={styles.buttonText}>
-                <Text style={styles.buttonTextTitle}>
-                    {data.Titulo}
-                </Text>
-                <Text style={styles.buttonTextSubtitle}>
-                    {data.Local}
-                </Text>
-                <Text style={styles.buttonTextSubtitle}>
-                    {data.Horario}
-                </Text>
+        <Text style={styles.buttonTextTitle}>{data.Titulo}</Text>
+        <Text style={styles.buttonTextSubtitle}>{data.Local}</Text>
+        <Text style={styles.buttonTextSubtitle}>{data.Horario}</Text>
       </View>
       <View style={styles.buttonIcons}>
         <View>
-
           <Image
             source={require('../../../assets/iconBell.png')}
             style={styles.bell}
-            />
-          <Text style={styles.circle}>
-            {data.bellCount} 
-          </Text>
+          />
         </View>
         <Image
           source={require('../../../assets/Pencil.png')}
@@ -35,33 +23,46 @@ const Button = function(data, index){
         />
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const MyEvents = () => {
-
-  //Recebe do backend as informações, 
-  let buttons = [{Titulo: 'Título do evento', Local: 'Local', Horario:'16:30-17:30',bellCount:5}, 
-                 {Titulo: 'Olimpiada', Local: 'Estônia', Horario:'29:30-47:30',bellCount:23},
-                 {Titulo: 'Show de fogos', Local: 'Coreia do Norte', Horario:'19:87-35:83',bellCount:12}
-                ];
+  //Recebe do backend as informações,
+  let buttons = [
+    {
+      Titulo: 'Título do evento',
+      Local: 'Local',
+      Horario: '16:30-17:30',
+      bellCount: 5
+    },
+    {
+      Titulo: 'Olimpiada',
+      Local: 'Estônia',
+      Horario: '29:30-47:30',
+      bellCount: 23
+    },
+    {
+      Titulo: 'Show de fogos',
+      Local: 'Coreia do Norte',
+      Horario: '19:87-35:83',
+      bellCount: 12
+    }
+  ];
 
   //Teste para multiplos botões
   buttons = buttons.concat(buttons).concat(buttons);
-  buttons = buttons.concat(buttons).concat(buttons)
+  buttons = buttons.concat(buttons).concat(buttons);
   // buttons = buttons.concat(buttons).concat(buttons)
 
   return (
-      <ScrollView 
-        style={styles.scroll}
-        contentContainerStyle={styles.eventDetailsContainer}
-      >
+    <ScrollView
+      style={styles.scroll}
+      contentContainerStyle={styles.eventDetailsContainer}
+    >
+      <Text style={styles.eventDetailsInfoTitle}>Meus Eventos</Text>
 
-        <Text style={styles.eventDetailsInfoTitle}>Meus Eventos</Text>
-
-        {buttons.map((data, index) => Button(data,index))}
-        
-      </ScrollView>
+      {buttons.map((data, index) => Button(data, index))}
+    </ScrollView>
   );
 };
 
